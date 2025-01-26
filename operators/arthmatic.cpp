e4bcfaa7ba0e4e1ba719d0c3ad08897974709d48
Ahtisham <lonesham86@gmail.com>
2025-01-26 22:21:55 +0530

    format arthmatic.cpp

--- operators/arthmatic.cpp
+++ operators/arthmatic.cpp
@@ -1,21 +1,24 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 int main()
-{ 
-     int TeaCups;
-     double  TeaPrice , totalprice , discountedPrice ;
-     cout<< " Enter the number of cups you need :"<< endl;
-     cin>> TeaCups;
-     cout<< " Enter the price per cup :"<< endl;
-     cin>> TeaPrice;
-     totalprice =TeaPrice*TeaCups;
-     // apply discount if total price is greater than 100;//
-     if(totalprice>100)
-     { discountedPrice = totalprice - (totalprice*0.05);
-     cout<< "discountedPrice is:"<<discountedPrice<<endl;}
-    else {
-        cout<< totalprice<<endl;
+{
+    int TeaCups;
+    double TeaPrice, totalprice, discountedPrice;
+    cout << " Enter the number of cups you need :" << endl;
+    cin >> TeaCups;
+    cout << " Enter the price per cup :" << endl;
+    cin >> TeaPrice;
+    totalprice = TeaPrice * TeaCups;
+    // apply discount if total price is greater than 100;//
+    if (totalprice > 100)
+    {
+        discountedPrice = totalprice - (totalprice * 0.05);
+        cout << "discountedPrice is:" << discountedPrice << endl;
     }
-    return 0 ;
+    else
+    {
+        cout << totalprice << endl;
+    }
+    return 0;
 }
