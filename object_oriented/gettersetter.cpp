385c2d0f28bcbe9f96a7db5983a193879da8bcea
Ahtisham <lonesham86@gmail.com>
2025-01-30 20:16:34 +0530

    gettersetter.cpp

--- object_oriented/gettersetter.cpp
+++ object_oriented/gettersetter.cpp
@@ -11,42 +11,70 @@ class chai{
     vector <string > ingredients ;
     public:
     chai(){
-        string =" unknown tea ";
-        servings = "2";
-        ingredients={"water","tealeaves","honey"};
+        teaName ="\t unknown tea ";
+        servings =  2 ;
+        ingredients={"water\n","tealeaves\n","honey\n"};
     }
-    chai(string name , int serve , vector<string>ingr){
-        teaName = name;
-        servings = serve;
-        ingredients = ingr ;
+    // chai(string name , int serve , vector<string>ingr){
+    //     teaName = name;
+    //     servings = serve;
+    //     ingredients = ingr ;
 
-    }
+    // }
 
 
     // getter  string // 
      string getTeaName(){
-            return teaName;
+        return teaName;
      }
 
      //settter//
      void setTeaName(string name ){
         //logic 
         teaName = name ;
-     }
-    void displaychaidetials (){
-        cout<<"teaname"<<teaName<<endl;
-        cout<<"servings"<<servings<<endl;
+        }
+       // getter servings //
+     int getServings(){
+        
+           return servings;
+       }
+        // setter servings //
+        void setServings(int serve){
+            servings=serve +2;
+        }
+        // getter  ingridients //
+        vector<string> getIngredients(){
+            return ingredients ;
+        }
+
+        // setter ingredients //
+        void setIngredients(vector<string>ingr){
+            ingr=ingredients;
+    
+        }
+     
+     void displaychaidetials (){
+        cout<<"teaname:"<<teaName<<endl;
+        cout<<"servings:"<<servings<<endl;
         cout<<"ingredients :";
             for (string ingredient : ingredients)
             {
             cout<<ingredient<< "";
-            }cout<< endl;
+            }
+            cout<< endl;
+    }
 };
 
-int main (){
+int main ()
+{
     chai chai;
-    chai.setTeaName("ginger tea ");
+    chai.setTeaName("masala tea ");
+    chai.getTeaName();
+    chai.setServings(5);
+    //chai.setIngredients();
+    
+    chai.displaychaidetials();
     
     
     return 0;
-};
+}
